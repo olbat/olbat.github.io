d735d250855486fd1089be72d004c1e1ac44fc1e
Luc Sarzyniec <devel@olbat.net>
2019-12-06 20:12:15 +0100

    Add a Rake task to generate a stripped version of main.js

---
+++ assets/js/_main.js
@@ -0,0 +1,78 @@
+/* ==========================================================================
+   jQuery plugin settings and other scripts
+   ========================================================================== */
+
+$(document).ready(function() {
+  // Sticky sidebar
+  var stickySideBar = function() {
+    var show =
+      $(".author__urls-wrapper button").length === 0
+        ? $(window).width() > 1024 // width should match $large Sass variable
+        : !$(".author__urls-wrapper button").is(":visible");
+    if (show) {
+      // fix
+      $(".sidebar").addClass("sticky");
+    } else {
+      // unfix
+      $(".sidebar").removeClass("sticky");
+    }
+  };
+
+  stickySideBar();
+
+  $(window).resize(function() {
+    stickySideBar();
+  });
+
+  // Follow menu drop down
+  $(".author__urls-wrapper button").on("click", function() {
+    $(".author__urls").toggleClass("is--visible");
+    $(".author__urls-wrapper button").toggleClass("open");
+  });
+
+  // Smooth scrolling
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    offset: 20,
+    speed: 400,
+    speedAsDuration: true,
+    durationMax: 500
+  });
+
+  // Gumshoe scroll spy init
+  if($("nav.toc").length > 0) {
+    var spy = new Gumshoe("nav.toc a", {
+      // Active classes
+      navClass: "active", // applied to the nav list item
+      contentClass: "active", // applied to the content
+
+      // Nested navigation
+      nested: false, // if true, add classes to parents of active link
+      nestedClass: "active", // applied to the parent items
+
+      // Offset & reflow
+      offset: 20, // how far from the top of the page to activate a content area
+      reflow: true, // if true, listen for reflows
+
+      // Event support
+      events: true // if true, emit custom events
+    });
+  }
+
+  // add lightbox class to all image links
+  $(
+    "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']"
+  ).addClass("image-popup");
+
+  // Add anchors for headings
+  $('.page__content').find('h1, h2, h3, h4, h5, h6').each(function() {
+    var id = $(this).attr('id');
+    if (id) {
+      var anchor = document.createElement("a");
+      anchor.className = 'header-link';
+      anchor.href = '#' + id;
+      anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><i class=\"fa fa-link\"></i>';
+      anchor.title = "Permalink";
+      $(this).append(anchor);
+    }
+  });
+});
